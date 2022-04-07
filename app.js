94230960515a3642249393df5bf8051e3e7a9f55
Evans Lin <j1996722@gmail.com>
2022-04-07 13:33:00 +0800

    get value,key,index in for loop

---
+++ app.js
@@ -0,0 +1,24 @@
+Vue.createApp({
+    data() {
+        return {
+            courses: [],
+            courseContent: "initial",
+            poop: {
+                name: "POOP",
+                duration: 35,
+                period: ['Sat', 'Sun'],
+                location: ['Taipei', 'Remote'],
+                instructor: {
+                    name: "Mark",
+                    location: 'Taipei'
+                }
+            }
+        }
+    },
+    computed: {},
+    methods: {
+        addCourse() {
+            this.courses.push(this.courseContent)
+        }
+    }
+}).mount("#app")
